87b64fa18d6be72440fec7c4906fdc92035aecab
Graham Reeves <graham@newchromantics.com>
2020-10-07 11:42:19 +0100

    Create bootup.js

---
+++ PopTestImage/bootup.js
@@ -0,0 +1,10 @@
+Pop.Debug(`Hello.`);
+
+const RedPixel = [255,0,0,255];
+const OutputImage = new Pop.Image();
+const Pixels = new Uint8Array(RedPixel);
+OutputImage.WritePixels( 1, 1, Pixels, 'RGBA' );
+const Png = OutputImage.GetPngData();
+
+//  output png to... stdout?
+throw `Now output file`;
